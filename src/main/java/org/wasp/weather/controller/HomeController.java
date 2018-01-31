fa9ffca3ef74c2bb57ce237e913c70865361e4b7
Raul Gonzalez <ragore.09@gmail.com>
2018-01-31 17:27:25 -0600

    Add logic for retrieving current location status

--- src/main/java/org/wasp/weather/controller/HomeController.java
+++ src/main/java/org/wasp/weather/controller/HomeController.java
@@ -4,8 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.wasp.weather.domain.Status;
 import org.wasp.weather.service.WeatherService;
 
@@ -26,13 +27,14 @@ public class HomeController {
         return "index";
     }
     
-    @RequestMapping(value = "/{lat},{lon}")
-    public String getLocation(@PathVariable("lat") Double latitude, 
-                              @PathVariable("lon") Double longitude) {
-        
+    @RequestMapping(value = "/getLocation")
+    public @ResponseBody Status getLocation(@RequestParam("lat") Double latitude, 
+                                            @RequestParam("lon") Double longitude) {
+        logger.info("im here");
         Status status = weatherService.getCurrentWeather(latitude, longitude);
         logger.info(status.toString());
-        return "index";
+        return status;
+//        return "";
     }
     
 }
--- src/main/js/weather.app.js
+++ src/main/js/weather.app.js
@@ -1,10 +1,19 @@
 import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 
+const rest = require('rest');
+
 class App extends Component {
     getGeoLocation() {
         if ("geolocation" in navigator) {
             console.log('geolocation available')
+            navigator.geolocation.getCurrentPosition(function(position) {
+                rest('/getLocation?lat='.concat(position.coords.latitude)
+                    .concat('&lon=').concat(position.coords.longitude))
+                    .then(function(response) { 
+                        console.log(response);
+                    });
+            });
         } else {
             console.log('geolocation not available')
         }
