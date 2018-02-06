243f309f694a391cc8963842418d12881dc6abf9
Raul Gonzalez <ragore.09@gmail.com>
2018-02-06 17:44:05 -0600

    add maps change of coordinates

--- src/main/js/weather.app.js
+++ src/main/js/weather.app.js
@@ -1,9 +1,20 @@
+'use strict';
+
 import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 
 const rest = require('rest');
 
 class App extends Component {
+    reallocateMap(status) {
+        const coordinates = new google.maps.LatLng({
+            lat: status.coord.lat, 
+            lng: status.coord.lon
+        });
+        map.panTo(coordinates);
+        map.setZoom(17);
+    }
+
     getGeoLocation() {
         if ("geolocation" in navigator) {
             console.log('geolocation available')
@@ -11,18 +22,18 @@ class App extends Component {
                 rest('/getLocation?lat='.concat(position.coords.latitude)
                     .concat('&lon=').concat(position.coords.longitude))
                     .then(function(response) { 
-                        console.log(response);
+                        let status = JSON.parse(response.entity);
+                        reallocateMap(status);
                     });
             });
         } else {
             console.log('geolocation not available')
         }
     }
-
+    
     render() {
         return (
             <div>
-                <div id="map"></div>
                 {this.getGeoLocation()}
             </div>
         );
