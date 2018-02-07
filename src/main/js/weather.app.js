5ed75297bc16424cff7a2af20caef9e164a2ca81
Raul Gonzalez <ragore.09@gmail.com>
2018-02-07 11:54:21 -0600

    Fix issue with anonymous function

--- src/main/js/weather.app.js
+++ src/main/js/weather.app.js
@@ -6,29 +6,34 @@ import ReactDOM from 'react-dom'
 const rest = require('rest');
 
 class App extends Component {
-    reallocateMap(status) {
-        const coordinates = new google.maps.LatLng({
-            lat: status.coord.lat, 
-            lng: status.coord.lon
-        });
-        map.panTo(coordinates);
-        map.setZoom(17);
+    constructor(props) { 
+        super(props);
     }
-
+    
     getGeoLocation() {
+        let self = this;
         if ("geolocation" in navigator) {
             console.log('geolocation available')
             navigator.geolocation.getCurrentPosition(function(position) {
                 rest('/getLocation?lat='.concat(position.coords.latitude)
                     .concat('&lon=').concat(position.coords.longitude))
-                    .then(function(response) { 
+                    .then((response) => { 
                         let status = JSON.parse(response.entity);
-                        reallocateMap(status);
+                        self.showMap(status);
                     });
             });
         } else {
             console.log('geolocation not available')
         }
+    }
+
+    showMap(status) { 
+        const coordinates = new google.maps.LatLng({
+            lat: status.coord.lat, 
+            lng: status.coord.lon
+        });
+        map.panTo(coordinates);
+        map.setZoom(14);
     }
     
     render() {
